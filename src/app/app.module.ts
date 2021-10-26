21d9928faf581b5c7e9ff1869449f2d41e0e4f7a
drocic <59014769+drocic@users.noreply.github.com>
2021-10-26 02:44:50 +0200

    10-26-21

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { appRoutingModule } from './app.routing'
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { SkillsComponent } from './skills/skills.component';
+import { StaticComponent } from './static/static.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    SkillsComponent,
+    StaticComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    appRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
